correct frame variance, action_lower_bound, theta as degree, and more loggers

buffer_control_ilqr.py
@@ -10,8 +10,12 @@ import params
 from params import Algo
 from params import ActionInitType
 from hidden_points_removal import HiddenPointsRemoval
-from dynamics import AutoDiffDynamics
-from cost import AutoDiffCost
+if params.SCALABLE_CODING:
+    from dynamics import AutoDiffDynamics
+    from cost import AutoDiffCost
+else:
+    from dynamics_nonscalable_coding import AutoDiffDynamics
+    from cost_nonscalable_coding  import AutoDiffCost
 
 np.random.seed(7)
 
@@ -31,8 +35,10 @@ class Buffer():
                  max_reg=1e10,
                  hessians=False):
 
-        self.logger = self.set_logger()
-        # self.plain_logger = logging.getLogger('plain_logger')
+        self.logger = self.set_logger(logger_level=logging.DEBUG)
+        self.logger_check_cost = self.set_logger(logger_name=__name__ +
+                                                 '.check_cost',
+                                                 logger_level=logging.INFO)
 
         self.buffer_length = params.BUFFER_LENGTH  # frame
 
@@ -229,10 +235,13 @@ class Buffer():
 
         #########################
 
-    def set_logger(self):
-        logger = logging.getLogger(__name__)
-        logger.propagate = False
-        logger.setLevel(logging.DEBUG)
+    def set_logger(self,
+                   logger_name=__name__,
+                   logger_level=logging.DEBUG,
+                   logger_propagate=False):
+        logger = logging.getLogger(logger_name)
+        logger.propagate = logger_propagate
+        logger.setLevel(logger_level)
         console_handler = logging.StreamHandler()
         formatter = logging.Formatter(
             '%(levelname)s - %(lineno)d - %(module)s\n%(message)s')
@@ -318,7 +327,7 @@ class Buffer():
         total_visible_size = np.sum(tiles_byte_sizes[visible_tiles_pos])
         distance_of_interest = distances[visible_tiles_pos]
 
-        theta_of_interest = params.TILE_SIDE_LEN / distance_of_interest
+        theta_of_interest = params.TILE_SIDE_LEN / distance_of_interest * params.RADIAN_TO_DEGREE
 
         a_tiles_of_interest = self.tile_a[frame_idx_within_video][
             visible_tiles_pos]
@@ -331,11 +340,14 @@ class Buffer():
         quality_tiles = self.cost.get_quality_for_tiles(
             num_points_per_degree, theta_of_interest)  # update quality_tiles
 
+        quality_tiles_in_mat = np.zeros_like(tiles_byte_sizes)
+        quality_tiles_in_mat[visible_tiles_pos] = quality_tiles
+
         frame_quality = np.sum(quality_tiles)
 
         ################# frame_quality_var ##########################
-        mask_prev_pop_frame = np.zeros_like(self.prev_pop_frame)
-        mask_prev_pop_frame[self.prev_pop_frame.nonzero()] = 1
+        mask_prev_pop_frame = np.zeros_like(self.tilesizes_prev_pop_frame)
+        mask_prev_pop_frame[self.tilesizes_prev_pop_frame.nonzero()] = 1
         mask_prev_visible_tiles = mask_prev_pop_frame * self.prev_viewing_probability
 
         mask_overlap_tiles_in_mat = mask_visible_tiles * mask_prev_visible_tiles
@@ -343,11 +355,12 @@ class Buffer():
         num_overlap_tiles = len(overlap_pos[0])
 
         if num_overlap_tiles != 0:
-            quality_diff = tiles_byte_sizes[overlap_pos] - self.prev_pop_frame[
+            quality_diff = quality_tiles_in_mat[overlap_pos] - self.tilequality_prev_pop_frame[
                 overlap_pos]
             frame_quality_var = np.sum(quality_diff**2) / num_overlap_tiles
         else:
             frame_quality_var = 0
+        self.tilequality_prev_pop_frame = quality_tiles_in_mat.copy()
 
         if len(viewing_probability.nonzero()[0]) == 0:
             self.mean_size_over_tiles_per_fov.append(float("inf"))
@@ -440,7 +453,8 @@ class Buffer():
 
             # pop processed frame and store it as previous frame
             # for calculation of quality_var for next frame
-            self.prev_pop_frame = self.buffer[0]
+            self.tilesizes_prev_pop_frame = self.buffer[0]
+            self.tilequality_prev_pop_frame = np.zeros_like(self.tilesizes_prev_pop_frame)
             self.prev_viewing_probability = viewing_probability[0].copy()
             self.buffer.pop(0)
 
@@ -1129,6 +1143,12 @@ class Buffer():
                     i * params.FPS:i * params.FPS +
                     params.TARGET_LATENCY].nonzero()
 
+                # new tiles are from last params.FPS frames in this forward round
+                new_tiles_into_buf_pos = viewing_probability[
+                    (i - 1) * params.FPS +
+                    params.TARGET_LATENCY:i * params.FPS +
+                    params.TARGET_LATENCY]
+
                 # TODO by Tongyu: correct rate_version_start_idx
                 rate_version_start_idx = (
                     update_start_idx + i * params.FPS) % params.BUFFER_LENGTH
@@ -1181,16 +1201,25 @@ class Buffer():
                 # 1e-3 is to avoid reaching upperbound
                 u_upper_bound = max_rates_cur_step[
                     tile_of_interest_pos] - 1e-3 - x
-                u_lower_bound = x - min_rates_cur_step[tile_of_interest_pos]
-                new_tiles_into_buffer = np.where(u_lower_bound <= 0)
-                num_new_tiles_into_buffer = len(new_tiles_into_buffer[0])
-                pos_old_states = np.where(u_lower_bound > 0)
-                u_lower_bound[pos_old_states] = 1e-3
-                u_lower_bound[new_tiles_into_buffer] = min_rates_cur_step[
-                    tile_of_interest_pos][(len(xs[i]) -
-                                           num_new_tiles_into_buffer):] + 1e-3
-                # if i == 9:
-                #     pdb.set_trace()
+
+                num_new_tiles_into_buffer = len(new_tiles_into_buf_pos[0])
+                num_old_tiles_in_buf = len(x) - num_new_tiles_into_buffer
+
+                u_lower_bound = np.zeros_like(x)
+                diff_old_states_with_minrate = min_rates_cur_step[
+                    tile_of_interest_pos][:
+                                          num_old_tiles_in_buf] - x[:
+                                                                    num_old_tiles_in_buf]
+                diff_old_states_with_minrate[
+                    diff_old_states_with_minrate < 0] = 0
+
+                # action lower bound for old tiles in buffer
+                u_lower_bound[:num_old_tiles_in_buf] += (
+                    diff_old_states_with_minrate + 1e-3)
+
+                # action lower bound for new tiles coming into buffer
+                u_lower_bound[num_old_tiles_in_buf:] = min_rates_cur_step[
+                    tile_of_interest_pos][num_old_tiles_in_buf:] + 1e-3
 
                 if params.ACTION_INIT_TYPE == ActionInitType.LOW:
                     us[i] = u_lower_bound.copy()
@@ -1423,6 +1452,9 @@ class Buffer():
         viewing_probability = np.array(viewing_probability)
         tile_of_interest_pos = viewing_probability[:params.
                                                    TARGET_LATENCY].nonzero()
+        new_tiles_into_buf_pos = viewing_probability[(
+            params.TARGET_LATENCY -
+            params.FPS):params.TARGET_LATENCY].nonzero()
         x0 = self.buffered_tiles_sizes[tile_of_interest_pos]
 
         # each tile has a 4 tuple location: (frame_idx, x, y, z)
@@ -1486,18 +1518,29 @@ class Buffer():
 
         ######## initialize action #########
         # TODO by Tongyu: add a function for action initialization
+        # TODO by Tongyu: try another kind of initialization: only last params.FPS frames are initialized as min_rates+1e-3, other tiles are just (1e-3)
 
         # 1e-3 is to avoid reaching upperbound
         u_upper_bound = self.max_rates_cur_step[
             tile_of_interest_pos] - 1e-3 - x0
-        u_lower_bound = x0 - self.min_rates_cur_step[tile_of_interest_pos]
-        new_tiles_into_buffer = np.where(u_lower_bound <= 0)
-        num_new_tiles_into_buffer = len(new_tiles_into_buffer[0])
-        pos_old_states = np.where(u_lower_bound > 0)
-        u_lower_bound[pos_old_states] = 1e-3
-        u_lower_bound[new_tiles_into_buffer] = self.min_rates_cur_step[
-            tile_of_interest_pos][(len(x0) -
-                                   num_new_tiles_into_buffer):] + 1e-3
+
+        num_new_tiles_into_buffer = len(new_tiles_into_buf_pos[0])
+        num_old_tiles_in_buf = len(x0) - num_new_tiles_into_buffer
+
+        u_lower_bound = np.zeros_like(x0)
+        diff_old_states_with_minrate = self.min_rates_cur_step[
+            tile_of_interest_pos][:
+                                  num_old_tiles_in_buf] - x0[:
+                                                             num_old_tiles_in_buf]
+        diff_old_states_with_minrate[diff_old_states_with_minrate < 0] = 0
+
+        # action lower bound for old tiles in buffer
+        u_lower_bound[:num_old_tiles_in_buf] += (diff_old_states_with_minrate +
+                                                 1e-3)
+
+        # action lower bound for new tiles coming into buffer
+        u_lower_bound[num_old_tiles_in_buf:] = self.min_rates_cur_step[
+            tile_of_interest_pos][num_old_tiles_in_buf:] + 1e-3
 
         if params.ACTION_INIT_TYPE == ActionInitType.LOW:
             us[0] = u_lower_bound.copy()

cost.py
@@ -82,16 +82,25 @@ class AutoDiffCost():
         self.weighted_quality_sum = 0.0
         self.quality_var = 0.0
         self.tile_utility_coef = params.TILE_UTILITY_COEF
+        self.quality_mat_prev_unupdateable_fr = np.zeros(
+            (params.NUM_TILES_PER_SIDE_IN_A_FRAME,
+             params.NUM_TILES_PER_SIDE_IN_A_FRAME,
+             params.NUM_TILES_PER_SIDE_IN_A_FRAME))
 
-        self.logger = self.set_logger()
+        self.logger = self.set_logger(logger_level=logging.INFO)
+        self.logger_check_cost = logging.getLogger(
+            'buffer_control_ilqr.check_cost')
 
         # regard all runtime warnings as errors, for debug use
         # warnings.simplefilter('error')
 
-    def set_logger(self):
-        logger = logging.getLogger(__name__)
-        logger.propagate = False
-        logger.setLevel(logging.DEBUG)
+    def set_logger(self,
+                   logger_name=__name__,
+                   logger_level=logging.DEBUG,
+                   logger_propagate=False):
+        logger = logging.getLogger(logger_name)
+        logger.propagate = logger_propagate
+        logger.setLevel(logger_level)
         console_handler = logging.StreamHandler()
         formatter = logging.Formatter(
             '%(levelname)s - %(lineno)d - %(module)s\n%(message)s')
@@ -202,8 +211,7 @@ class AutoDiffCost():
             self.updated_tiles_pos = self.viewing_probability[
                 i * params.FPS:i * params.FPS +
                 params.TARGET_LATENCY].nonzero()
-            # self.logger.debug('updated_tiles_pos %s', self.updated_tiles_pos)
-            # self.logger.debug(':start_frame_idx_within_video %s', start_frame_idx_within_video)
+            # self.logger.debug('updated_tiles_pos: %s\nstart_frame_idx_within_video: %d', self.updated_tiles_pos, start_frame_idx_within_video)
 
             update_time_step = self.update_start_idx // params.FPS
             # buffer is not fully occupied yet
@@ -272,7 +280,7 @@ class AutoDiffCost():
             self.frame_weights = np.ones_like(self.tiles_rates_to_be_watched)
         ### TODO by Tongyu: add more frame weight types
 
-        self.theta_of_interest = params.TILE_SIDE_LEN / self.distance_of_interest
+        self.theta_of_interest = params.TILE_SIDE_LEN / self.distance_of_interest * params.RADIAN_TO_DEGREE
         self.num_points = self.a_updated_tiles * self.tiles_rates_to_be_watched + self.b_updated_tiles
         self.num_points_per_degree = self.num_points**0.5 / self.theta_of_interest  # f_ang
         self.quality_tiles = self.get_quality_for_tiles(
@@ -312,21 +320,61 @@ class AutoDiffCost():
         self.order_nonzero_view_prob_of_interest = self.order_nonzero_view_prob_of_interest.astype(
             int)
 
-        tile_of_interest_pos_prev_unupdateable_fr = buffer_[params.FPS -
-                                                            1].nonzero()
-        viewing_probability_prev_unupdateable_fr = np.zeros_like(
-            buffer_[params.FPS - 1])
-        viewing_probability_prev_unupdateable_fr[
-            tile_of_interest_pos_prev_unupdateable_fr] = 1
+        # FIXME [URGENT] by Tongyu: not buffer_ here, should be quality of previous frame
+        # if i==0: recalculate quality matrix of previous frame, elif i>0:refer to self.prev_fr_quality_tiles_in_mat
+        if i == 0:
+            tile_of_interest_pos_prev_unupdateable_fr = buffer_[params.FPS -
+                                                                1].nonzero()
+            viewing_probability_prev_unupdateable_fr = np.zeros_like(
+                buffer_[params.FPS - 1])
+            viewing_probability_prev_unupdateable_fr[
+                tile_of_interest_pos_prev_unupdateable_fr] = 1
 
-        self.mask_overlap_tiles_in_mat[
-            0] = viewing_probability_prev_unupdateable_fr * self.viewing_probability_of_interest[
-                0]
-        self.quality_diff[0] = (
-            buffer_[params.FPS - 1] -
-            self.quality_tiles_in_mat[0]) * self.mask_overlap_tiles_in_mat[0]
-        self.num_overlap_tiles_adjacent_frames[0] = np.count_nonzero(
-            self.quality_diff[0])
+            self.mask_overlap_tiles_in_mat[
+                0] = viewing_probability_prev_unupdateable_fr * self.viewing_probability_of_interest[
+                    0]
+            self.num_overlap_tiles_adjacent_frames[0] = np.count_nonzero(
+                self.mask_overlap_tiles_in_mat[0])
+            # if self.num_overlap_tiles_adjacent_frames[0] != 0:
+
+            #     # calculate tile_quality_mat of previous frame
+            #     distance_of_interest = distances[visible_tiles_pos]
+
+            #     theta_of_interest = params.TILE_SIDE_LEN / distance_of_interest * params.RADIAN_TO_DEGREE
+
+            #     a_tiles_of_interest = self.tile_a[frame_idx_within_video][
+            #         visible_tiles_pos]
+            #     b_tiles_of_interest = self.tile_b[frame_idx_within_video][
+            #         visible_tiles_pos]
+            #     tiles_rates_to_be_watched = tiles_byte_sizes[visible_tiles_pos]
+
+            #     num_points = a_tiles_of_interest * tiles_rates_to_be_watched + b_tiles_of_interest
+            #     num_points_per_degree = num_points**0.5 / theta_of_interest  # f_ang
+            #     quality_tiles = self.cost.get_quality_for_tiles(
+            #         num_points_per_degree, theta_of_interest)  # update quality_tiles
+
+            #     quality_tiles_in_mat = np.zeros_like(tiles_byte_sizes)
+            #     quality_tiles_in_mat[visible_tiles_pos] = quality_tiles
+
+            #     self.quality_diff[0] = (
+            #         buffer_[params.FPS - 1] -
+            #         self.quality_tiles_in_mat[0]) * self.mask_overlap_tiles_in_mat[0]
+        else:
+            tile_of_interest_pos_prev_unupdateable_fr = self.quality_mat_prev_unupdateable_fr.nonzero(
+            )
+            viewing_probability_prev_unupdateable_fr = np.zeros_like(
+                self.quality_mat_prev_unupdateable_fr)
+            viewing_probability_prev_unupdateable_fr[
+                tile_of_interest_pos_prev_unupdateable_fr] = 1
+
+            self.mask_overlap_tiles_in_mat[
+                0] = viewing_probability_prev_unupdateable_fr * self.viewing_probability_of_interest[
+                    0]
+            self.num_overlap_tiles_adjacent_frames[0] = np.count_nonzero(
+                self.mask_overlap_tiles_in_mat[0])
+        self.quality_mat_prev_unupdateable_fr = self.quality_tiles_in_mat[
+            params.FPS - 1].copy()
+
         if self.num_overlap_tiles_adjacent_frames[0] != 0:
             self.quality_var_adjacent_frames[0] = np.sum(
                 self.quality_diff[0]**
@@ -408,7 +456,19 @@ class AutoDiffCost():
         self.sum_u = np.sum(u)
         self.bw_bound_barrier = -np.log(self.bandwidth_budget[i] - self.sum_u)
 
+        # TODO by Tongyu: weights of barrier function should be as small as possible! maybe no need to adjust inner weights of them. e.g., 1e-6 * zero_bound_barrier
+        # if do so, all the derivatives also need to be modified with weights
         self.current_cost = -self.weighted_quality_sum + self.sum_quality_var * params.WEIGHT_VARIANCE + self.zero_bound_barrier + self.upper_bound_barrier + self.lower_bound_barrier + self.bw_bound_barrier
+
+        if not np.isnan(self.current_cost):
+            self.logger_check_cost.debug(
+                'weighted quality sum: %f\nsum quality var: %f\nzero bound barrier: %f\nupper bound barrier: %f\nlower bound barrier: %f\nbw bound barrier: %f',
+                -self.weighted_quality_sum, self.sum_quality_var,
+                self.zero_bound_barrier, self.upper_bound_barrier,
+                self.lower_bound_barrier, self.bw_bound_barrier)
+
+            if self.sum_quality_var > 1e4:
+                pdb.set_trace()
 
         return self.current_cost
 

params.py
@@ -29,6 +29,9 @@ class FrameWeightType(Enum):
     EXP_DECREASE = 2
     FOV_PRED_ACCURACY = 3  # based on fov overlap ratio between pred and true
 
+SCALABLE_CODING = True
+
+RADIAN_TO_DEGREE = 180 / np.pi
 
 WEIGHT_VARIANCE = 1
 
@@ -48,7 +51,7 @@ SVC_OVERHEAD = 1
 SMOOTH_MIN_PARAM = 50
 
 FPS = 30
-TARGET_LATENCY = 90  # in frame
+TARGET_LATENCY = 30  # in frame
 ILQR_HORIZON = TARGET_LATENCY // FPS
 
 # Assume frame is independently encoded/decoded